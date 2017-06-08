8865e6e001cfd663c3aabefed91ad347b4a715ac
Alexander Dejonghe <alexander.dejonghe@intec.ugent.be>
2017-06-08 11:08:14 +0200

    use /parliament as volume + query updates

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ git clone -b c-geosparql https://github.com/adejonghe/rsp-services-csparql.git
 
 git clone -b caprads https://github.com/adejonghe/TripleWave.git
 
-export PARLIAMENT_LIB=$POG_DEMO/CSPARQL-engine/csparql-sparql-parliament/lib
+#export PARLIAMENT_LIB=$POG_DEMO/CSPARQL-engine/csparql-sparql-parliament/lib
 
 cd $POG_DEMO/CSPARQL-engine && mvn clean install
 
